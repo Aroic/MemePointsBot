530267420a19b4b5ba16560f774b659f0ca0e7b8
Trevor Martin <tmart44@gmail.com>
2019-01-19 11:26:26 -0800

    Added fuzzy search, working dictionaries.

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
 const Discord = require("discord.js");
 const auth = require('.././auth.json');
 const util = require('util');
+const fuzzy = require('fuzzyset.js');
+
 
 const client = new Discord.Client();
 const prefix = "!";
+const nameConfidence = Number(auth.confidence);
 
 //The specific ID of piwi.
 const PIWI_GUILD_ID = auth.guild_id;
@@ -26,49 +29,57 @@ client.on("message", async message => {
 	
 	//ignore messages not meant for us
 	if (message.content.indexOf(prefix) !== 0) return;
-	
-	//Assumes message format of <cmd> <user> <points>
-	const args = message.content.slice(prefix.length).trim().split(" ");
+	var channel = message.channel;
+	//Assumes message format of <cmd> <user>:<points>
+	const args = message.content.slice(prefix.length).trim().split(":");
 	const cmd = args.shift().toLowerCase();
-	var reply;
-	
+	var returnArr = [];
+	var reply = "Something went wrong, did you format correctly? !<cmd>:<user>:<points> ";
+	console.log(util.format("Starting message fn\ncmd = %s\nargs = %s | %d", cmd, args[0], args[1]));
 	if (cmd === "memepoints" || cmd === "mp"){
 		//Assigning Meme points
-		if (userNames.indexOf(args[0]) === -1){
-			reply = (`${args[0]} is not a user in this discord. Try again.`);
-		}
-		else if (isNaN(args[1])){
-			reply = (`${args[0]} is not a valid number`);
+		//Scrub input. 
+		debugger;
+		returnArr = scrubInput(args[0], args[1]);
+		console.log(util.format("(MP)Scrubbed Successfully, returned=>\nuser:%s | points:%d", returnArr[0], returnArr[1]));
+		//If we passed, apply points and send message.
+		if (returnArr[1] === true){		
+			var userName = returnArr[0];
+			var points = Number(args[1]);
+			MemeDictionary[userName] += points;
+			reply = (`${userName} has gained ${points} Meme points.`)
 		}
 		else{
-			var index = userNames.indexOf(args[0]);
-			MemeDictionary[index] += Number(args[1]);
-			reply = (`${args[0]} has gained ${args[1]} Meme points.`)
-		}
-		
+			//We failed, let them know.
+			reply = returnArr[0];
+		}		
 	}
 	if (cmd === "toxicpoints" || cmd === "tp"){
-		//Assigning toxic points
-		if (userNames.indexOf(args[0]) === -1){
-			reply = (`${args[0]} is not a user in this discord. Try again.`);
-		}
-		else if (isNaN(args[1])){
-			reply = (`${args[0]} is not a valid number`);
+		//Assigning Toxic points
+		//Scrub input. 
+		returnArr = scrubInput(args[0], args[1]);
+		console.log(util.format("(TP)Scrubbed Successfully, returned=>\nuser:%s | points:%d", returnArr[0], returnArr[1]));
+		//If we passed, apply points and send message.
+		if (returnArr[1] === true){		
+			var userName = returnArr[0];
+			var points = Number(args[1]);
+			ToxicDictionary[userName] += points;
+			reply = (`${userName} has gained ${points} Toxic points.`);
 		}
 		else{
-			var index = userNames.indexOf(args[0]);
-			ToxicDictionary[index] += Number(args[1]);
-			reply = (`${args[0]} has gained ${args[1]} Toxic points.`)
-		}
+			//We failed, let them know.
+			reply = returnArr[0];
+		}		
 	}
 	if (cmd === "ranks" || cmd === "r"){
 		//Show all scores
-		reply = (`\n\tName\t|\tMemePoints\t|\tToxicPoints\n`)
+		reply = (`\n\tName\t|\tMemePoints\t|\tToxicPoints\n`);
 		for (var i = 0; i < our_guild.memberCount; i++ ){
-			reply += (`\t${userNames[i]}\t:\t${MemeDictionary[i]}\t|\t${ToxicDictionary[i]}\n`)
+			reply += (`\t${userNames[i]}\t:\t${MemeDictionary[userNames[i]]}\t|\t${ToxicDictionary[userNames[i]]}\n`);
 		}
 	}
-	message.reply(reply);
+	channel.send(reply)
+	.catch(console.error);
 });
 
 client.login(auth.token);
@@ -79,13 +90,46 @@ function FillDictionary(){
 		if (our_guild.memberCount > 0){
 			console.log(`Member count: ${our_guild.memberCount}`);
 			var arr = our_guild.members.map(m=>m.user.username);
-			var arrUserNames = our_guild.members.map(m=>m.user.username);
 			for (var i = 0; i < our_guild.memberCount; i++){
 				console.log(arr[i]);
-				MemeDictionary[i] = Number(0);				
-				ToxicDictionary[i] = Number(0);
+				MemeDictionary[arr[i]] = Number(0);				
+				ToxicDictionary[arr[i]] = Number(0);
 				userNames[i] = arr[i];
 			}
 		}
 	}
+}
+
+//Scrub the input.
+//Returns an array of (string, bool), if bool is false
+//then the input is bad and just return an error message.
+
+function scrubInput(user, points){
+	console.log(util.format("Scrubbing User Input: %s : %d", user, points));
+	var userName = ApproxName(user);
+	if (userName === null){
+		reply = (`Couldn't find ${user} in this discord! Try again.`);
+		return [reply, false];
+	}
+	if (isNaN(points)){
+		reply = (`${points} is not a valid number! Try again.`);
+		return [reply, false];
+	}
+	// If we're here, the username and points are valid.
+	return [userName, true];
+	
+}
+
+//Find approx user with fuzzy search.
+//If no valid name is found, return undef.
+//fuzzy.get returns a double array [[score, username]]
+function ApproxName(nameToSearch){
+	console.log("Applying FuzzySearch to %s ", nameToSearch)
+	var fuzzyData = FuzzySet(userNames);
+	var result = fuzzyData.get(nameToSearch, null, nameConfidence);	
+	if (result === null){
+		return result;
+	}
+	console.log(util.format("Result of fuzzy: %s, %d ", result[0][0], result[0][1]))
+	return result[0][1];
 }
